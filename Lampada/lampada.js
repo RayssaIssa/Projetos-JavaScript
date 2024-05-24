2a12b0c0e4f20c5e5e99e010500ad088834e9fd9
Rayssa Costa <115506404+RayssaIssa@users.noreply.github.com>
2024-05-24 17:49:16 -0300

    Update lampada.js

--- Lampada/lampada.js
+++ Lampada/lampada.js
@@ -1,27 +1,33 @@
 const lampada = document.getElementById("lampada");
-
-const ligar = document.getElementById("ligar");
-const desligar = document.getElementById("desligar");
+const interruptor = document.getElementById("interruptor");
 
 function taQuebrada(){
     return lampada.src.indexOf('quebrada') > -1;
 }
+function OnOff(){
+    if(interruptor.textContent == 'Ligar'){
+        lampOn();
+    }else{
+        lampOff();
+    }
+}
 function lampOn(){
     if(!taQuebrada()){
         lampada.src = 'img/ligada.jpg';
+        interruptor.textContent = 'Desligar';
     }
 }
 function lampOff(){
     if(!taQuebrada()){
         lampada.src = 'img/desligada.jpg';
+        interruptor.textContent = 'Ligar';
     }
 }
 function quebrada(){
     lampada.src = 'img/quebrada.jpg';
 }
 
-ligar.addEventListener('click', lampOn);
-desligar.addEventListener('click', lampOff);
+interruptor.addEventListener('click', OnOff);
 
 lampada.addEventListener('mouseover', lampOn);
 lampada.addEventListener('mouseleave', lampOff);
